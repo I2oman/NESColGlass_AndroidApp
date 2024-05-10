f3df746774115f1ab05b96c18ac8516d5a237295
Copciop23 <138177233+Copciop23@users.noreply.github.com>
2024-05-10 13:40:32 +0100

    Animation works on first 2 fragments. But it doesn't function with the settings page will message more about the problem.

---
+++ app/src/main/java/com/example/nescolglass/MapsPage.java
@@ -0,0 +1,22 @@
+package com.example.nescolglass;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class MapsPage extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+}
